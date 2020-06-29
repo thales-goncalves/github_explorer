4eb362b6acf6f4d786970ae1ee63957d2d48e294
Thales Gonçalves <thales.g.goncalves@outlook.com>
2020-06-29 16:40:05 -0300

    Add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
+import Routes from './routes'
 
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Ola</h1>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+)
 
 export default App;
